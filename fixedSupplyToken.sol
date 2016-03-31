605df17a326bf6f04576a79f07b7699e1282431f
benwaah <benoit.wagler@gmail.com>
2016-03-31 18:18:26 +0200

    Updated formatting to follow Solidity official style guide

--- fixedSupplyToken.sol
+++ fixedSupplyToken.sol
@@ -1,82 +1,75 @@
-contract token
-{
-	function transfer(address receiver, uint amount) {}
+contract Token {
+    function transfer(address receiver, uint amount) {}
 }
 
-contract Crowdsale
-{
-	address public beneficiary;
-	uint public fundingGoal;
-	uint public amountRaised;
-	uint public deadline;
-	uint public price;
-	token public tokenReward;
-	Funder[] public funders;
-	bool crowdsaleClosed;
 
-	event FundTransfer(address backer, uint amount, bool isContribution);
+contract Crowdsale {
+    address public beneficiary;
+    uint public fundingGoal;
+    uint public amountRaised;
+    uint public deadline;
+    uint public price;
+    Token public tokenReward;
+    Funder[] public funders;
+    bool crowdsaleClosed;
 
-	/* Data structure to hold information about campain contributors */
-	struct Funder
-	{
-		address addr;
-		uint amount;
-	}
+    event FundTransfer(address backer, uint amount, bool isContribution);
 
-	/* At initialization, setup the owner */
-	function Crowdsale(address ifSuccessfulSendTo,
-					   uint fundingGoalInEthers,
-					   uint durationInMinutes,
-					   uint etherCostOfEachToken,
-					   token addressOfTokenUsedAsReward)
-	{
-		beneficiary = ifSuccessfulSendTo;
-		fundingGoal = fundingGoalInEthers * 1 ether;
-		deadline = now + durationInMinutes * 1 minutes;
-		price = etherCostOfEachToken * 1 ether;
-		tokenReward = token(addressOfTokenUsedAsReward);
-	}
+    /* Data structure to hold information about campain contributors */
+    struct Funder {
+        address addr;
+        uint amount;
+    }
 
-	/* The function without name is the default function that is called whenever anyone sends funds to a contract */
-	function ()
-	{
-		if (crowdsaleClosed)
-			throw;
-		uint amount = msg.value;
-		funders[funders.length++] = Funder({addr: msg.sender, amount: amount});
-		amountRaised += amount;
-		tokenReward.transfer(msg.sender, amount / price);
-		FundTransfer(msg.sender, amount, true);
-	}
+    /* At initialization, setup the owner */
+    function Crowdsale(
+        address ifSuccessfulSendTo,
+        uint fundingGoalInEthers,
+        uint durationInMinutes,
+        uint etherCostOfEachToken,
+        token addressOfTokenUsedAsReward
+    ) {
+        beneficiary = ifSuccessfulSendTo;
+        fundingGoal = fundingGoalInEthers * 1 ether;
+        deadline = now + durationInMinutes * 1 minutes;
+        price = etherCostOfEachToken * 1 ether;
+        tokenReward = token(addressOfTokenUsedAsReward);
+    }
 
-	modifier afterDeadLine()
-	{
-		if (now >= deadline)
-			_
-	}
+    /* The function without name is the default function that is called whenever anyone sends funds to a contract */
+    function () {
+        if (crowdsaleClosed)
+            throw;
+        uint amount = msg.value;
+        funders[funders.length++] = Funder({addr: msg.sender, amount: amount});
+        amountRaised += amount;
+        tokenReward.transfer(msg.sender, amount / price);
+        FundTransfer(msg.sender, amount, true);
+    }
 
-	/* Checks if the goal or time limit has been reached and ends the campain */
-	function checkGoalReached() afterDeadLine
-	{
-		if (crowdsaleClosed)
-			throw;
+    modifier afterDeadLine() {
+        if (now >= deadline)
+            _
+    }
 
-		if (amountRaised > fundingGoal)
-		{
-			beneficiary.send(amountRaised);
-			FundTransfer(beneficiary, amountRaised, false);
-		}
-		else
-		{
-			for (uint i = 0; i < funders.length; i++)
-			{
-				funders[i].addr.send(funders[i].amount);
-				FundTransfer(funders[i].addr, funders[i].amount, false);
-			}
-		}
+    /* Checks if the goal or time limit has been reached and ends the campain */
+    function checkGoalReached() afterDeadLine {
+        if (crowdsaleClosed)
+            throw;
 
-		// Send any remaining balance to beneficiary anyway
-		beneficiary.send(this.balance);
-		crowdsaleClosed = true;
-	}
+        if (amountRaised > fundingGoal) {
+            beneficiary.send(amountRaised);
+            FundTransfer(beneficiary, amountRaised, false);
+        }
+        else {
+            for (uint i = 0; i < funders.length; i++) {
+                funders[i].addr.send(funders[i].amount);
+                FundTransfer(funders[i].addr, funders[i].amount, false);
+            }
+        }
+
+        // Send any remaining balance to beneficiary anyway
+        beneficiary.send(this.balance);
+        crowdsaleClosed = true;
+    }
 }
--- test.sol
+++ test.sol
@@ -1,174 +1,164 @@
-contract owned
-{
-	address public owner;
+contract Owned {
+    address public owner;
 
-	function owned()
-	{
-		owner = msg.sender;
-	}
+    function Owned() {
+        owner = msg.sender;
+    }
 
-	modifier onlyOwner
-	{
-		if (msg.sender != owner)
-			throw;
-		_
-	}
+    modifier onlyOwner {
+        if (msg.sender != owner)
+            throw;
+        _
+    }
 
-	function transferOwnership(address newOwner) onlyOwner
-	{
-		owner = newOwner;
-	}
+    function transferOwnership(address newOwner) onlyOwner {
+        owner = newOwner;
+    }
 }
 
-contract MyToken is owned
-{
-	/* Public variables of the token */
-	string public name;
-	string public symbol;
-	uint8 public decimals;
 
-	uint256 public sellPrice;
-	uint256 public buyPrice;
+contract MyToken is Owned {
+    /* Public variables of the token */
+    string public name;
+    string public symbol;
+    uint8 public decimals;
 
-	uint minBalanceForAccounts;
+    uint256 public sellPrice;
+    uint256 public buyPrice;
 
-	/* This creates an array with all balances */
-	mapping (address => uint256) public balanceOf;
-	mapping (address => bool) public frozenAccount;
-	mapping (address => bool) public approvedAccount;
+    uint minBalanceForAccounts;
 
-	/* This generates a public event on the blockchain that will notify clients */
-	event Transfer(address indexed from, address indexed to, uint256 value);
-	event FrozenFunds(address target, bool frozen);
-	event ApprovedFunds(address target, bool approve);
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceOf;
+    mapping (address => bool) public frozenAccount;
+    mapping (address => bool) public approvedAccount;
 
-	/* Initializes contract with initial supply tokens to the creator of the contract */
-	function MyToken(uint256 initialSupply, string tokenName,
-					string tokenSymbol, uint8 decimalUnits,
-					address centralMinter)
-	{
-		// If supply not given then generate one million
-		if (initialSupply == 0)
-			initialSupply = 1000000;
-		// Give the creator all initial tokens
-		balanceOf[msg.sender] = initialSupply;
+    /* This generates a public event on the blockchain that will notify clients */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event FrozenFunds(address target, bool frozen);
+    event ApprovedFunds(address target, bool approve);
 
-		// Set the name for display purposes
-		name = tokenName;
-		// Set the symbol for display purposes
-		symbol = tokenSymbol;
-		// Amount of decimals for display purposes
-		decimals = decimalUnits;
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function MyToken(
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol,
+        uint8 decimalUnits,
+        address centralMinter
+    ) {
+        // If supply not given then generate one million
+        if (initialSupply == 0)
+            initialSupply = 1000000;
+        // Give the creator all initial tokens
+        balanceOf[msg.sender] = initialSupply;
 
-		// Set ownership if specified
-		if (centralMinter != 0)
-			owner = msg.sender;
-	}
+        // Set the name for display purposes
+        name = tokenName;
+        // Set the symbol for display purposes
+        symbol = tokenSymbol;
+        // Amount of decimals for display purposes
+        decimals = decimalUnits;
 
-	/* Send coins */
-	function transfer(address _to, uint256 _value)
-	{
-		/* Check if the sender has a frozen account */
-		if (frozenAccount[msg.sender])
-			throw;
+        // Set ownership if specified
+        if (centralMinter != 0)
+            owner = msg.sender;
+    }
 
-		/* Check if the sender if approved for transfer */
-		if (!approvedAccount[msg.sender])
-			throw;
+    /* Send coins */
+    function transfer(address _to, uint256 _value) {
+        /* Check if the sender has a frozen account */
+        if (frozenAccount[msg.sender])
+            throw;
 
-		/* Check if the sender has enough balance */
-		if (balanceOf[msg.sender] < _value)
-			throw;
+        /* Check if the sender if approved for transfer */
+        if (!approvedAccount[msg.sender])
+            throw;
 
-		/* Check for overflows */
-		if (balanceOf[_to] + _value < balanceOf[_to])
-			throw;
+        /* Check if the sender has enough balance */
+        if (balanceOf[msg.sender] < _value)
+            throw;
 
-		/* Add and substract new balances */
-		balanceOf[msg.sender] -= _value;
-		balanceOf[_to] += _value;
+        /* Check for overflows */
+        if (balanceOf[_to] + _value < balanceOf[_to])
+            throw;
 
-		/* Notify anyone listening that this transfer took place */
-		Transfer(msg.sender, _to, _value);
+        /* Add and substract new balances */
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
 
-		// Check if the sender still owns enough ether
-		if (msg.sender.balance < minBalanceForAccounts)
-		sell((minBalanceForAccounts - msg.sender.balance) / sellPrice);
-		/* Another option is to pay the fee to the receiver by the sender:
-		if (_to.balance < minBalanceForAccounts)
-			_to.send(sell((minBalanceForAccounts - _to.balance) / sellPrice));
-		*/
-	}
+        /* Notify anyone listening that this transfer took place */
+        Transfer(msg.sender, _to, _value);
 
-	function mintToken(address _target, uint256 _mintedAmount) onlyOwner
-	{
-		balanceOf[_target] += _mintedAmount;
-		Transfer(0, _target, _mintedAmount);
-	}
+        // Check if the sender still owns enough ether
+        if (msg.sender.balance < minBalanceForAccounts)
+        sell((minBalanceForAccounts - msg.sender.balance) / sellPrice);
+        /* Another option is to pay the fee to the receiver by the sender:
+        if (_to.balance < minBalanceForAccounts)
+            _to.send(sell((minBalanceForAccounts - _to.balance) / sellPrice));
+        */
+    }
 
-	function freezeAccount(address _target, bool _freeze) onlyOwner
-	{
-		frozenAccount[_target] = _freeze;
-		FrozenFunds(_target, _freeze);
-	}
+    function mintToken(address _target, uint256 _mintedAmount) onlyOwner {
+        balanceOf[_target] += _mintedAmount;
+        Transfer(0, _target, _mintedAmount);
+    }
 
-	function approveAccount(address _target, bool _approve) onlyOwner
-	{
-		approvedAccount[_target] = _approve;
-		ApprovedFunds(_target, _approve);
-	}
+    function freezeAccount(address _target, bool _freeze) onlyOwner {
+        frozenAccount[_target] = _freeze;
+        FrozenFunds(_target, _freeze);
+    }
 
-	function setPrices(uint256 _newSellPrice, uint256 _newBuyPrice) onlyOwner
-	{
-		sellPrice = _newSellPrice;
-		buyPrice = _newBuyPrice;
-	}
+    function approveAccount(address _target, bool _approve) onlyOwner {
+        approvedAccount[_target] = _approve;
+        ApprovedFunds(_target, _approve);
+    }
 
-	function buy() returns (uint amount)
-	{
-		// Calculate the amount
-		amount = msg.value / buyPrice;
-		// Checks if it has enough to buy
-		if (balanceOf[this] < amount)
-			throw;
-		// Adds the amount to buyer's balance
-		balanceOf[msg.sender] += amount;
-		// Substracts the amount from seller's balance
-		balanceOf[this] -= amount;
-		// Execute an event reflecting the change
-		Transfer(this, msg.sender, amount);
-		// Ends function and returns
-		return amount;
-	}
+    function setPrices(uint256 _newSellPrice, uint256 _newBuyPrice) onlyOwner {
+        sellPrice = _newSellPrice;
+        buyPrice = _newBuyPrice;
+    }
 
-	function sell(uint amount) returns (uint revenue)
-	{
-		// Check if the sender has enough to sell
-		if (balanceOf[msg.sender] < amount)
-			throw;
-		// Adds the amount to the owner's balance
-		balanceOf[this] += amount;
-		// Substracts the amount from seller's balance
-		balanceOf[msg.sender] -= amount;
-		// Calculate the revenue
-		revenue = amount * sellPrice;
-		// Sends ether to the seller
-		msg.sender.send(revenue);
-		// Executes an event reflecting the change
-		Transfer(msg.sender, this, amount);
-		// Ends function and returns
-		return revenue;
-	}
+    function buy() returns (uint amount) {
+        // Calculate the amount
+        amount = msg.value / buyPrice;
+        // Checks if it has enough to buy
+        if (balanceOf[this] < amount)
+            throw;
+        // Adds the amount to buyer's balance
+        balanceOf[msg.sender] += amount;
+        // Substracts the amount from seller's balance
+        balanceOf[this] -= amount;
+        // Execute an event reflecting the change
+        Transfer(this, msg.sender, amount);
+        // Ends function and returns
+        return amount;
+    }
 
-	function setMinBalance(uint minimumBalanceInFinney) onlyOwner
-	{
-		minBalanceForAccounts = minimumBalanceInFinney * 1 finney;
-	}
+    function sell(uint amount) returns (uint revenue) {
+        // Check if the sender has enough to sell
+        if (balanceOf[msg.sender] < amount)
+            throw;
+        // Adds the amount to the owner's balance
+        balanceOf[this] += amount;
+        // Substracts the amount from seller's balance
+        balanceOf[msg.sender] -= amount;
+        // Calculate the revenue
+        revenue = amount * sellPrice;
+        // Sends ether to the seller
+        msg.sender.send(revenue);
+        // Executes an event reflecting the change
+        Transfer(msg.sender, this, amount);
+        // Ends function and returns
+        return revenue;
+    }
 
-	/* This unnamed function is called whenever someone tries to send ether to it */
-	function ()
-	{
-		// Prevent accidental sending of ether
-		throw;
-	}
+    function setMinBalance(uint minimumBalanceInFinney) onlyOwner {
+        minBalanceForAccounts = minimumBalanceInFinney * 1 finney;
+    }
+
+    /* This unnamed function is called whenever someone tries to send ether to it */
+    function () {
+        // Prevent accidental sending of ether
+        throw;
+    }
 }
